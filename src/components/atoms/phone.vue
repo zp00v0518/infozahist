e3ef08969fb48a8def3e48c8fbf09e35810ae4d4
Dmitriy <you@example.com>
2019-09-05 08:43:58 +0300

    create phone

--- src/components/atoms/phone.vue
+++ src/components/atoms/phone.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="hello">
- phone
+  <div class="string">
+    <Vtitle :title="data.title" :required="isRequred"></Vtitle>
+    <div>
+      <span v-if="isError" class="error">{{error}}</span>
+      <ElementInput v-model="value" @change="handlerChange" />
+    </div>
   </div>
 </template>
+
+<script>
+import { Input as ElementInput } from "element-ui";
+import { validation, baseMixin } from "./mixins";
+
+export default {
+  name: "Vphone",
+  components: { ElementInput },
+  mixins: [validation, baseMixin]
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/app.scss";
+</style>
